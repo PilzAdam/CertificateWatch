d8c005a14bf5b38ba8ce1aeef281f5108a5cd5fb
PilzAdam <pilzadam@minetest.net>
2019-08-16 21:46:15 +0200

    Remove now-unused escapeHtml

--- common.js
+++ common.js
@@ -15,16 +15,6 @@ function convertDate(unix){
 			+ ":" + date.getSeconds().toString().padStart(2, "0");
 }
 
-// from https://stackoverflow.com/questions/6234773/can-i-escape-html-special-chars-in-javascript
-function escapeHtml(unsafe) {
-	return unsafe
-		.replace(/&/g, "&amp;")
-		.replace(/</g, "&lt;")
-		.replace(/>/g, "&gt;")
-		.replace(/"/g, "&quot;")
-		.replace(/'/g, "&#039;");
-}
-
 // formats bytes like "15.32 MiB"
 function formatBytes(bytes) {
 	const prefixes = ["B", "KiB", "MiB", "GiB", "TiB"];
